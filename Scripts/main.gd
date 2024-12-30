ae8829aadb703721560d9072ceb53c9c2255649f
Niamh Carter <niamh.carter13@gmail.com>
2024-12-30 10:20:58 +0000

    Added blocks

---
+++ Scripts/main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+var block_scene = preload("res://Scenes/Block.tscn")
+
+
+func _ready() -> void:
+	for i in range(1, 17):
+		for j in range(1, 9):
+			var block = block_scene.instantiate() as StaticBody2D
+			var x = 15 + 4.8 * i + (65 * (i - 1))
+			var y = 15 + 4.8 * j + (30 * (j - 1))
+			block.position = Vector2(x, y)
+			add_child(block)
